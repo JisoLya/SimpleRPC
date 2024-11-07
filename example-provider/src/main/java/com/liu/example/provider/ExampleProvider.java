38e9a2a461ce68ff135846cfc2344ae6c0bc4d53
liuyan <SoLiuyan@163.com>
2024-11-07 15:04:01 +0800

    添加配置文件的读取部分，支持application.properties(目前)

--- example-provider/src/main/java/com/liu/example/provider/ExampleProvider.java
+++ example-provider/src/main/java/com/liu/example/provider/ExampleProvider.java
@@ -1,11 +1,15 @@
 package com.liu.example.provider;
 
 import com.liu.example.common.service.UserService;
+import com.liu.rpc.RpcApplication;
 import com.liu.rpc.registry.LocalRegistry;
 import com.liu.rpc.server.VertxHttpServer;
 
-public class ServiceProvider {
+public class ExampleProvider {
     public static void main(String[] args) {
+        //配置初始化
+        RpcApplication.init();
+
         //服务的注册
         LocalRegistry.registry(UserService.class.getName(),UserServiceImpl.class);
 
