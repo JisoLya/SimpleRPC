197fa9a7c49ae86f8689eb8695a7837d99099e72
liuyan <SoLiuyan@163.com>
2024-11-08 16:33:22 +0800

    优化了配置文件的读取

--- soyan-rpc-core/src/main/java/com/liu/rpc/utils/ConfigUtils.java
+++ soyan-rpc-core/src/main/java/com/liu/rpc/utils/ConfigUtils.java
@@ -8,6 +8,7 @@ import org.w3c.dom.Document;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Objects;
 
 public class ConfigUtils {
 
@@ -34,42 +35,20 @@ public class ConfigUtils {
      */
     public static <T> T loadConfig(Class<T> tClass, String prefix, String environment) {
         //TODO 监听配置文件的变更
-        StringBuilder builder = new StringBuilder("application");
-        if (StrUtil.isNotBlank(environment)) {
-            builder.append("-").append(environment);
-        }
         //1. 查找带有application-environment前缀的文件
-        String file = ConfigUtils.class.getClassLoader().getResource("").getFile();
+        String configFileName = getConfigFileName();
+        return PropertiesReader.getProps(prefix,configFileName,tClass);
+    }
+    
+    private static String getConfigFileName(){
+        String file = Objects.requireNonNull(ConfigUtils.class.getClassLoader().getResource("")).getFile();
         File target = new File(file);
-        String configFileName = null;
-        for (File f : target.listFiles()) {
+        String configFileName = "";
+        for (File f : Objects.requireNonNull(target.listFiles())) {
             if (f.getName().startsWith("application")) {
                 configFileName = f.getName();
             }
         }
-        if (configFileName.endsWith(".properties")) {
-            builder.append(".properties");
-            Props props = new Props(builder.toString());
-
-            return props.toBean(tClass, prefix);
-        } else if (configFileName.endsWith(".xml")) {
-            try {
-                return (T) XmlReader.parseXmltoObject(file + configFileName,prefix);
-            } catch (DocumentException e) {
-                e.printStackTrace();
-            }
-        } else if (configFileName.endsWith(".yaml")||configFileName.endsWith(".yml")) {
-            try {
-                return (T) YmlReader.parseYmlToObject(file+configFileName,prefix);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-        System.out.println("Undefined file suffix!");
-        return null;
-
-        //2. 分析其后缀，调用不同的文件读取器
-        //3. 封装到Bean，返回
+        return configFileName;
     }
-
 }
--- soyan-rpc-core/src/main/java/com/liu/rpc/utils/PropertiesReader.java
+++ soyan-rpc-core/src/main/java/com/liu/rpc/utils/PropertiesReader.java
@@ -1,21 +1,83 @@
 package com.liu.rpc.utils;
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.setting.dialect.Props;
+import com.liu.rpc.config.RpcConfig;
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
 
 public class PropertiesReader {
     public static <T> T getProps(String prefix, String fileName, Class<T> clazz) {
-
         if (fileName.endsWith(".properties")) {
             Props props = new Props(fileName);
             return props.toBean(clazz, prefix);
         } else if (fileName.endsWith(".xml")) {
             //1. 创建XML读取器
-            //2. 转化为props对象
+            File file = FileUtil.file(fileName);
+            SAXReader reader = new SAXReader();
+            try {
+                Document document = reader.read(file);
+                Element root = document.getRootElement();
 
+                Props props = new Props();
+                parseElement(root, "", props);
+
+                return props.toBean(clazz, prefix);
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            }
+            //2. 转化为props对象
         } else if (fileName.endsWith(".yml") || fileName.endsWith(".yaml")) {
-            Props props = new Props(fileName);
-            return props.toBean(clazz, prefix);
+            File file = FileUtil.file(fileName);
+            Yaml yaml = new Yaml();
+
+            try (FileInputStream inputStream = new FileInputStream(file)) {
+                Map<String, Object> data = yaml.load(inputStream);
+                Props props = new Props();
+                convertMaptoProps(data, "", props);
+                return props.toBean(clazz, prefix);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        return null;
+        return (T) new RpcConfig();
+    }
+
+    private static void parseElement(Element element, String prefix, Props props) {
+        String key = prefix.isEmpty() ? element.getName() : prefix + "." + element.getName();
+        String value = element.getTextTrim();
+
+        for (Attribute attribute : element.attributes()) {
+            props.setProperty(key + "." + attribute.getName(), attribute.getValue());
+        }
+
+        if (value.isEmpty() && !element.elements().isEmpty()) {
+            for (Element child : element.elements()) {
+                parseElement(child, key, props);
+            }
+        } else {
+            props.setProperty(key, value);
+        }
+    }
+
+    private static void convertMaptoProps(Map<String, Object> data, String prefix, Props props) {
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            String key = prefix.isEmpty() ? entry.getKey() : prefix + "." + entry.getKey();
+            Object value = entry.getValue();
+            if (value instanceof Map) {
+                convertMaptoProps((Map<String, Object>) value, key, props);
+            } else {
+                props.setProperty(key, value);
+            }
+        }
     }
 }
